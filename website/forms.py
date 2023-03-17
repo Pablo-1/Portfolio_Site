f49714b745c141e30f10a19b7a58d44794944820
Pawel Gonczarewicz <p.gonczarewicz@gmail.com>
2023-03-17 14:11:12 +0000

    Finished 1.0

--- website/forms.py
+++ website/forms.py
@@ -22,3 +22,4 @@ class FeedbackForm(forms.ModelForm):
                 'placeholder':'Your feedback goes here'
             })
         }
+
--- website/views.py
+++ website/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
+from django.shortcuts import redirect
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', context)
+# def home(request):
+#     form = FeedbackForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = FeedbackForm()
 
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'home.html', context)
+
+def home(request):
+    form = FeedbackForm()
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = FeedbackForm()
+            return redirect('home')
+        else:
+            form = FeedbackForm()
+    return render(request, 'home.html', {'form':form})
+
+# def send_form(request):
+#     form = FeedbackForm()
+#     if request.method == "POST":
+#         form = FeedbackForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#         else:
+#             form = FeedbackForm()
+    
+#     return render(request, 'sucess.html', {'form':form})
